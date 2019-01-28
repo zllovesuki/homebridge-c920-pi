9db8f7efbc2cc8811ba369215037e9bdf57a0b2c
Rachel Chen <me@rachelchen.me>
2019-01-28 13:36:08 -0800

    fixed

--- ffmpeg.js
+++ ffmpeg.js
@@ -276,46 +276,19 @@ FFMPEG.prototype.handleStreamRequest = function(request) {
         let audioKey = sessionInfo["audio_srtp"];
         let audioSsrc = sessionInfo["audio_ssrc"];
 
-        let ffmpegCommand = this.ffmpegSource + ' -map 0:0' +
-          ' -vcodec ' + vcodec +
-          ' -pix_fmt yuv420p' +
-          ' -r ' + fps +
-          ' -f rawvideo' +
-          ' ' + additionalCommandline +
-          ' -vf scale=' + width + ':' + height +
-          ' -b:v ' + vbitrate + 'k' +
-          ' -bufsize ' + vbitrate+ 'k' +
-          ' -maxrate '+ vbitrate + 'k' +
-          ' -payload_type 99' +
-          ' -ssrc ' + videoSsrc +
-          ' -f rtp' +
-          ' -srtp_out_suite AES_CM_128_HMAC_SHA1_80' +
-          ' -srtp_out_params ' + videoKey.toString('base64') +
-          ' srtp://' + targetAddress + ':' + targetVideoPort +
-          '?rtcpport=' + targetVideoPort +
-          '&localrtcpport=' + targetVideoPort +
-          '&pkt_size=' + packetsize;
-
-        if(this.audio){
-          ffmpegCommand+= ' -map 0:1' +
-            ' -acodec ' + acodec +
-            ' -profile:a aac_eld' +
-            ' -flags +global_header' +
-            ' -f null' +
-            ' -ar ' + asamplerate + 'k' +
-            ' -b:a ' + abitrate + 'k' +
-            ' -bufsize ' + abitrate + 'k' +
-            ' -ac 1' +
-            ' -payload_type 110' +
-            ' -ssrc ' + audioSsrc +
-            ' -f rtp' +
-            ' -srtp_out_suite AES_CM_128_HMAC_SHA1_80' +
-            ' -srtp_out_params ' + audioKey.toString('base64') +
-            ' srtp://' + targetAddress + ':' + targetAudioPort +
-            '?rtcpport=' + targetAudioPort +
-            '&localrtcpport=' + targetAudioPort +
-            '&pkt_size=' + packetsize;
-        }
+        let ffmpegCommand = '-f rtsp -vcodec h264_mmal -i rtsp://127.0.0.1:8555/unicast -map 0:0 ' +
+	                    '-vcodec h264_omx -r ' + fps + ' -vf scale=' + width + ':' + height + ' -b:v ' + vbitrate + 'k -bufsize ' + vbitrate + 'k ' +
+	                    '-payload_type 99 -ssrc ' + videoSsrc + ' -f rtp -srtp_out_suite AES_CM_128_HMAC_SHA1_80 -srtp_out_params ' +
+	                    videoKey.toString('base64') + ' srtp://' + targetAddress + ':' + targetVideoPort +
+                        '?rtcpport=' + targetVideoPort +
+                        '&localrtcpport=' + targetVideoPort +
+                        '&pkt_size=' + packetsize +
+	                    ' -map 0:1 -acodec libfdk_aac -profile:a aac_eld -flags +global_header -f null -b:a ' + abitrate + 'k -ar ' + asamplerate + 'k' +
+	                    '-bufsize ' + abitrate + 'k -ac 1 -payload_type 110 -ssrc ' + audioSsrc + ' -f rtp -srtp_out_suite ' +
+	                    'AES_CM_128_HMAC_SHA1_80 -srtp_out_params ' + audioKey.toString('base64') + ' srtp://' + targetAddress + ':' + targetAudioPort +
+                        '?rtcpport=' + targetAudioPort +
+                        '&localrtcpport=' + targetAudioPort +
+                        '&pkt_size=' + packetsize;
 
         let ffmpeg = spawn(this.videoProcessor, ffmpegCommand.split(' '), {env: process.env});
         this.log("Start streaming video from " + this.name + " with " + width + "x" + height + "@" + vbitrate + "kBit");
